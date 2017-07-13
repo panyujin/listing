847b08b156f65f32afde01bd4cf63933f9715f73
panyujin <panyujinyx@gmail.com>
2017-07-13 14:59:06 +0800

    implement part4

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :jobs
   end
-  
+
   devise_for :users
 
   resources :jobs
-  root 'welcome#index'
+  #root 'welcome#index'
+  root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
