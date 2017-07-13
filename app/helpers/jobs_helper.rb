403cfac299c54825337664a257ab4bd4067cfa02
panyujin <panyujinyx@gmail.com>
2017-07-13 17:27:29 +0800

    implement no 5

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -8,4 +8,12 @@ module JobsHelper
        end
      end
 
+     def render_job_status(job)
+       if job.is_hidden
+         content_tag(:span, "", :class => "fa fa-lock")
+       else
+         content_tag(:span, "", :class => "fa fa-globe")
+       end
+     end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,15 @@ Rails.application.routes.draw do
     resources :jobs
   end
 
+  namespace :admin do
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
+    end
+  end
+
   devise_for :users
 
   resources :jobs
