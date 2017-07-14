43876c595c3ed6e180785d087f266b2bb39483b7
panyujin <panyujinyx@gmail.com>
2017-07-14 11:10:23 +0800

    no 07 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :jobs
+  resources :jobs do
+    resources :resumes
+  end
   #root 'welcome#index'
   root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
